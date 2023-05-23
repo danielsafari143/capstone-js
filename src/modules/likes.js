8a6a87be8bf7df9c7a52ada862663e0f657196dc
danielsafari143 <danielsafari143@gmail.com>
2023-05-23 08:36:39 +0200

    fix errors

--- src/modules/likes.js
+++ src/modules/likes.js
@@ -5,11 +5,11 @@ import updateLikes from './updateLike.js';
 const likes = async (idRef) => {
   const val = parseInt(idRef, 10);
   if (!Number.isNaN(val)) {
-    await sendLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/sc87C4sjE8VQg9VVNXwH/likes/', val);
+    await sendLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/S8JH3xcckzqE06YNqkme/likes/', val);
     const ids = document.getElementById(idRef);
     const parent = ids.parentNode;
     parent.style.color = 'red';
-    await updateLikes(fetchLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/sc87C4sjE8VQg9VVNXwH/likes/'));
+    await updateLikes(fetchLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/S8JH3xcckzqE06YNqkme/likes/'));
   }
 };
 
