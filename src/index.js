c149ffd3e7359bcf16d94ed61089f5431ed82ede
danielsafari143 <danielsafari143@gmail.com>
2023-05-23 09:33:38 +0200

    change the api id

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import likes from './modules/likes.js';
 import fetchLikes from './modules/fetchLikes.js';
 import updateLikes from './modules/updateLike.js';
 
-displayListItems(fetchingData('https://api.tvmaze.com/search/shows?q=girls'), fetchLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/S8JH3xcckzqE06YNqkme/likes/'));
-await updateLikes(fetchLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/S8JH3xcckzqE06YNqkme/likes/'));
+displayListItems(fetchingData('https://api.tvmaze.com/search/shows?q=girls'));
+await updateLikes(fetchLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DonU6QfOk4iiEnTnnZtN/likes/'));
 
 window.addEventListener('click', async (e) => {
   likes(e.target.id);
--- src/modules/likes.js
+++ src/modules/likes.js
@@ -5,11 +5,11 @@ import updateLikes from './updateLike.js';
 const likes = async (idRef) => {
   const val = parseInt(idRef, 10);
   if (!Number.isNaN(val)) {
-    await sendLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/S8JH3xcckzqE06YNqkme/likes/', val);
+    await sendLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DonU6QfOk4iiEnTnnZtN/likes/', val);
     const ids = document.getElementById(idRef);
     const parent = ids.parentNode;
     parent.style.color = 'red';
-    await updateLikes(fetchLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/S8JH3xcckzqE06YNqkme/likes/'));
+    await updateLikes(fetchLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DonU6QfOk4iiEnTnnZtN/likes/'));
   }
 };
 
