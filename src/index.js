3d52fba70619912186da86817949136ed7a3c55d
danielsafari143 <danielsafari143@gmail.com>
2023-05-23 12:12:13 +0200

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './style.css';
+import displayListItems from './modules/displayListItems.js';
+import fetchingData from './modules/ferchData.js';
+import likes from './modules/likes.js';
+import fetchLikes from './modules/fetchLikes.js';
+import updateLikes from './modules/updateLike.js';
 
-const searchShow = () => {
-  const url = 'https://api.tvmaze.com/shows/1/episodes';
-  fetch(url)
-    .then((response) => response.json())
-    .then((jsonData) => {
-      const result = jsonData.map((element) => element);
-      console.log(result);
-    });
-};
+displayListItems(fetchingData('https://api.tvmaze.com/search/shows?q=girls'));
+await updateLikes(fetchLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DonU6QfOk4iiEnTnnZtN/likes/'));
 
-searchShow();
+window.addEventListener('click', async (e) => {
+  likes(e.target.id);
+});
