143dd593e23b386d6bbbbb215be226a74bcd29fb
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-05-24 11:11:12 +0500

    Fix Code and update the header and footer section

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-import displayListItems from './modules/displayListItems.js';
-import fetchingData from './modules/ferchData.js';
-import likes from './modules/likes.js';
-import fetchLikes from './modules/fetchLikes.js';
-import updateLikes from './modules/updateLike.js';
 
-displayListItems(fetchingData('https://api.tvmaze.com/search/shows?q=girls'));
-await updateLikes(fetchLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DonU6QfOk4iiEnTnnZtN/likes/'));
+import fetchData from './modules/getShowsList.js';
 
-window.addEventListener('click', async (e) => {
-  likes(e.target.id);
-});
+fetchData();
